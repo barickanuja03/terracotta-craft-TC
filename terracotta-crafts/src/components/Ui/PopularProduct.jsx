4983fc8333c3ffee71633159318d50fde70a7ec0
barickanuja03 <anujabarick001@gmail.com>
2025-03-08 10:29:39 +0530

    Product added

--- terracotta-crafts/src/components/Ui/PopularProduct.jsx
+++ terracotta-crafts/src/components/Ui/PopularProduct.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Popularcard from './Popularcard';
 import { NavLink } from 'react-router-dom'
 
 const PopularProduct = () => {
@@ -12,13 +13,16 @@ const PopularProduct = () => {
               </div>
               <p className="h-1 w-30 bg-blue-700 rounded-full  mb-12"></p>
         </article>
-        <div className='flex flex-row justify-center'>
+          <div className='grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 h-auto gap-x-3 gap-y-5 my-10'>
+            <Popularcard />
+            </div>
+            <div className='flex flex-row justify-center w-full'>
             <span className="button">
               <NavLink to="/product">
                 View all products
               </NavLink>
             </span>
-        </div>
+           </div>
     </section>
   )
 }
